205e8b4978463f73ef0e800f750fb3d2a816659b
Артём Соколовский <artem.1990.sokolovskiy@gmail.com>
2018-04-15 23:26:45 +0300

    assert corrected

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,16 +1,9 @@
 
-import io.restassured.config.SSLConfig;
-import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
-import java.util.ArrayList;
-
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.put;
 import static io.restassured.RestAssured.when;
 
 public class APITest extends BaseTest{
@@ -50,17 +43,8 @@ public class APITest extends BaseTest{
         Assert.assertEquals(lukePlanetResponse.path("population"), "200000", "Check if population is correct");
         Assert.assertEquals(filmResponse.path("title"), "Attack of the Clones", "Check if file title is correct");
         Assert.assertTrue(getValue(filmResponse, "characters").contains(LUKE_URL), "Check if character is exist in film");
-        Assert.assertTrue(getValue(filmResponse, "planets").contains(getValue(lukePlanetResponse, "films").get(0)), "Check if planet is exist in film");
+        Assert.assertTrue(getValue(filmResponse, "planets").contains(lukePlanet), "Check if planet is exist in film");
 
-
-
-
-       //response.path("homeworld");
-
-
-
-
-       //
 
 
 
